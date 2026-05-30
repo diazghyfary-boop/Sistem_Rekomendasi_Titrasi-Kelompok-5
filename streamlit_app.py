3ff5dca2edabab6ac13c1669d134cb408433394e
diazghyfary-boop <diazghyfary@gmail.com>
2026-05-31 00:10:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-flex = st.container(vertical=True, vertical_alignment="left")
+Title=print("SELAMAT DATANG!")
+flex = st.container(horizontal=True, horizontal_alignment="right")
 
 for card in range(3):
     flex.button(f"Button {card + 1}")
 
 Copy
-
